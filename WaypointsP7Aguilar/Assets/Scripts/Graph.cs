487eceb983807ce041c0575275955767c7f99432
Kimberly Aguilar <145504527+Moociie@users.noreply.github.com>
2025-01-10 15:25:34 -0800

    need last section of lesson
    finished simple API

--- WaypointsP7Aguilar/Assets/Scripts/Graph.cs
+++ WaypointsP7Aguilar/Assets/Scripts/Graph.cs
@@ -1,8 +1,10 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 
-public class Graph 
+public class Graph
 {
     List<Edge> edges = new List<Edge>();
     List<Node> nodes = new List<Node>();
@@ -20,7 +22,7 @@ public class Graph
         Node from = FindNode(fromNode);
         Node to = FindNode(toNode);
 
-        if(from != null && != null)
+        if (from != null && to != null)
         {
             Edge e = new Edge(from, to);
             edges.Add(e);
@@ -31,9 +33,107 @@ public class Graph
     {
         foreach (Node n in nodes)
         {
-            if(n.getId() == id)
+            if (n.getId() == id)
                 return n;
         }
         return null;
     }
-}
+    public bool AStar(GameObject startId, GameObject endId)
+    {
+        Node start = FindNode(startId);
+        Node end = FindNode(endId);
+
+        if (start == null || end == null)
+        {
+            return false;
+        }
+        List<Node> open = new List<Node>();
+        List<Node> closed = new List<Node>();
+        float tentative_g_score = 0;
+        bool tentative_is_better;
+
+        start.g = 0;
+        start.h = distance(start, end);
+
+        open.Add(start);
+        while (open.Count > 0)
+        {
+            int i = lowestF(open);
+            Node thisNode = open[i];
+            if (thisNode.getId() == endId)
+            {
+                return true;
+            }
+
+            open.RemoveAt(i);
+            closed.Add(thisNode);
+            Node neighbor;
+            foreach (Edge e in thisNode.edgeList)
+            {
+                neighbor = e.endNode;
+
+                if (closed.IndexOf(neighbor) > -1)
+                    continue;
+
+                tentative_g_score = thisNode.g + distance(thisNode, neighbor);
+                if (open.IndexOf(neighbor) == -1)
+                {
+                    open.Add(neighbor);
+                    tentative_is_better = true;
+                }
+                else if (tentative_g_score < neighbor.g)
+                {
+                    tentative_is_better = true;
+                }
+                else
+                    tentative_is_better = false;
+
+                if (tentative_is_better)
+                {
+                    neighbor.cameFrom = thisNode;
+                    neighbor.g = tentative_g_score;
+                    neighbor.h = distance(thisNode, end);
+                    neighbor.f = neighbor.g + neighbor.h;
+                }
+            }
+        }
+        return false;
+    }
+        public void ReconstructionPath(Node startId, Node endId)
+    {
+        pathList.Clear();
+        pathList.Add(endId);
+
+        var p = endId.cameFrom;
+        while (p != startId && p != null)
+        {
+            pathList.Insert(0, p);
+            p = p.cameFrom;
+        }
+        pathList.Insert(0, startId);
+    }
+
+        float distance(Node a, Node b)
+        {
+            return (Vector3.SqrMagnitude(a.getId().transform.position - b.getId().transform.position));
+        }
+        int lowestF(List<Node> l)
+        {
+            float lowestf = 0;
+            int count = 0;
+            int iteratorCount = 0;
+
+            lowestf = l[0].f;
+
+            for (int i = 1; i < l.Count; i++)
+            {
+                if (l[i].f <= lowestf)
+                {
+                    lowestf = l[i].f;
+                    iteratorCount = count;
+                }
+                count++;
+            }
+            return iteratorCount;
+        }
+    }
